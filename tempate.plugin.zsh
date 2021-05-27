8652affd8f56374da66bf703e016bc4d631943a7
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-05-27 12:56:50 -0500

    Change `subl` to `$EDITOR` (#3)
    * Make $EDITOR handle edit steps
    
    Sublime text is hardcoded into the original plugin files, but many don't have Sublime text. This edit simply changes every reference from `subl` to `$EDITOR`, which makes it far more conducive to generic use, as well as use on a headless system.
    
    * Cleanup extra whitespace

--- tempate.plugin.zsh
+++ tempate.plugin.zsh
@@ -1,13 +1,13 @@
 "__${template}_help () {
-    printf 'Example usage:                     
-  ${template} help, h           - show help 
+    printf 'Example usage:
+  ${template} help, h           - show help
 '
 }
 
 ${template}_runner() {
-  if [[ \$@ == \"help\" || \$@ == \"h\" ]]; then 
+  if [[ \$@ == \"help\" || \$@ == \"h\" ]]; then
     __${template}_help
-  #elif [[ \$@ == \"something\" || \$@ == \"alias\" ]]; then 
+  #elif [[ \$@ == \"something\" || \$@ == \"alias\" ]]; then
   fi
 }
 
